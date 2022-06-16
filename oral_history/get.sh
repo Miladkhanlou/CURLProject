28b89494c08fedf26047bb4c1a082f2b264753a2
Miladkhanlou <milad.khl90@gmail.com>
2022-06-16 10:07:08 -0500

    new curl to get oral history datastreams from the ldl

---
+++ oral_history/get.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -o rels-ext.rdf https://louisianadigitallibrary.org/islandora/object/lsu-thwcoh-lsuhistory%3A21/datastream/RELS-EXT/download
+curl -o mods.xml https://louisianadigitallibrary.org/islandora/object/lsu-thwcoh-lsuhistory%3A21/datastream/MODS/download
+curl -o obj.mp3 https://louisianadigitallibrary.org/islandora/object/lsu-thwcoh-lsuhistory%3A21/datastream/OBJ/download
+curl -o pdf-transcript.pdf https://louisianadigitallibrary.org/islandora/object/lsu-thwcoh-lsuhistory%3A21/datastream/PDF-TRANSCRIPT/download
+curl -o techmd.xml https://louisianadigitallibrary.org/islandora/object/lsu-thwcoh-lsuhistory%3A21/datastream/TECHMD/download
